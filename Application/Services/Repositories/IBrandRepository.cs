c1ee695465cf78d7f8de4b872edf11c5c3d9dad0
Sinan <sinanfen@hotmail.com>
2023-08-28 23:57:20 +0300

    refactor

--- Application/Services/Repositories/IBrandRepository.cs
+++ Application/Services/Repositories/IBrandRepository.cs
@@ -1,13 +1,9 @@
 ﻿using Core.Persistence.Repositories;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Services.Repositories;
 
 public interface IBrandRepository : IAsyncRepository<Brand, Guid>, IRepository<Brand, Guid>
 {
+
 }
