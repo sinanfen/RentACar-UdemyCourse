f438a7a9449c527af9ad80f82b12c8e6b75fce27
Sinan <sinanfen@hotmail.com>
2023-09-08 23:59:14 +0300

    model entity - carState enum added

---
+++ Domain/Entities/Model.cs
@@ -0,0 +1,37 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Model : Entity<Guid>
+{
+    public int BrandId { get; set; }
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+}
+
+public class Transmission : Entity<Guid>
+{
+    public string Name { get; set; }
+}
+
+public class Fuel : Entity<Guid>
+{
+    public string Name { get; set; }
+}
+
+public class Car : Entity<Guid>
+{
+    public int Model { get; set; }
+    public int Kilometer { get; set; }
+    public short ModelYear { get; set; }
+    public string Plate { get; set; }
+    public short MinFindexScore { get; set; }
+    public CarState CarState { get; set; }
+}
