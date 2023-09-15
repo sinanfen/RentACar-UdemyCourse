33404c793b1388b5cccc91bd50ac0df8702a599a
Sinan <sinanfen@hotmail.com>
2023-09-15 23:56:25 +0300

    .

--- Application/Features/Brands/Constants/BrandsMessages.cs
+++ Application/Features/Brands/Constants/BrandsMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Features.Brands.Constants;
+﻿namespace Application.Features.Brands.Constants;
 
 public class BrandsMessages
 {
